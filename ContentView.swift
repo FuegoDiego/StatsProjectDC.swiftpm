8e13ae5fc2e5eb981264ea8c9359e38ec8b76670
diego.chavez27 <noicedude555@gmail.com>
2025-11-10 06:55:31 -0600

    basically added all initial tasks from trello board

--- ContentView.swift
+++ ContentView.swift
@@ -1,30 +1,90 @@
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 struct ContentView: View {
     @State var selectedDay = 1
     @State var selectedMonth = "January"
-    @State var selectedInstrument = "Gutair"
-    @State var selectedAmtTime = 5
+    @State var selectedInstrument = "Guitar"
+    @State var selectedAmtTime = 15
     @State var logList: [Log] = []
     @Query var qLogs: [Log]
     @Environment(\.modelContext) var context
     var body: some View {
-        VStack {
-            Text("Instrument Practice Logs")
-            List{
-                Text("Placeholder")
-                ForEach(qLogs){ item in
-                    
-                }
-            }
-           
-        }
-            
+        //Gold: Color(red: 212/255, green: 175/255, blue: 55/255)
         NavigationView {
-            NavigationLink("Add a Log"){
-                PracticeView(selectedDay: $selectedDay, selectedMonth: $selectedMonth, logList: $logList))
+            VStack {
+                Text("Instrument Practice Logs")
+                    .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                    .bold()
+                    .font(.title)
+                List {
+                    //Text("Placeholder")
+                    ForEach(qLogs) { log in
+                        
+                        VStack(alignment: .leading) {
+                            Text(log.month + " \(log.day)")
+                                .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                .bold()
+                                .font(.title3)
+                            Text("-------------")
+                                .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                .bold()
+                                .font(.title3)
+                            Text("Practiced on \(log.instrument)")
+                                .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                .bold()
+                            
+                            if log.amtPractice % 60 == 0 {
+                                Text(
+                                    "Practiced for \(log.amtPractice/60) hours"
+                                )
+                                .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                .bold()
+                                
+                            } else if Double(log.amtPractice) / 60 > 1 {
+                                Text(
+                                    "Practiced for \(Double(log.amtPractice)/60, specifier: "%.1f") hours"
+                                )
+                                .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                .bold()
+                                
+                            } else {
+                                Text("Practiced for \(log.amtPractice) minutes")
+                                    .foregroundColor(Color(red: 212/255, green: 175/255, blue: 55/255))
+                                    .bold()
+                            }
+                        
+                        }
+                        .listRowBackground(Color.black)
+                    }
+                    .onDelete(perform: deleteLog)
+                    
+
+                }
+                .scrollContentBackground(.hidden)
+                .background(Color(red: 64/255, green: 64/255, blue: 64/255))
+
+                NavigationLink("Add a Log +") {
+                    PracticeView(
+                        selectedDay: $selectedDay,
+                        selectedMonth: $selectedMonth,
+                        logList: $logList,
+                        selectedInstrument: $selectedInstrument,
+                        selectedAmtTime: $selectedAmtTime
+                    )
+                }
+                .foregroundColor(.white)
+                .bold()
             }
+            .background(Color.black)
+        }
+        
+    }
+    func deleteLog(at offsets: IndexSet) {
+        for index in offsets {
+            let thing = qLogs[index]
+            context.delete(thing)
+            try? context.save()
         }
     }
 }
--- PracticeView.swift
+++ PracticeView.swift
@@ -15,6 +15,8 @@ struct PracticeView: View {
     @State var days = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     @State var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     @State var instruments = ["Guitar", "Violin", "Flute", "Clarinet", "Saxophone", "Trumpet", "French Horn", "Trombone", "Drum Set", "Piano"]
+    @State var amtOfTimes = [15, 30, 45, 60, 90, 120, 150, 180]
+    @Environment(\.modelContext) var context
     @Binding var logList: [Log]
     @Binding var selectedInstrument: String
     @Binding var selectedAmtTime: Int
@@ -23,29 +25,78 @@ struct PracticeView: View {
     var body: some View {
         VStack{
             Text("Please enter date")
+                .foregroundStyle(Color(red: 212/255, green: 175/255, blue: 55/255))
+                .bold()
             HStack{
-                Picker("Enter Day", selection: $selectedDay){
-                    ForEach(0..<days.count, id: \.self){ i in
-                        Text("\(days[i])")
+                Picker("Select Day", selection: $selectedDay){
+                    ForEach(days, id: \.self){ i in
+                        Text("\(days[i-1])")
                             .tag(i)
+                            
                     }
                 }
+                .accentColor(.white)
+                .bold()
                 
-                Picker("Enter Month", selection: $selectedMonth){
+                Picker("Select Month", selection: $selectedMonth){
                     ForEach(allMonths, id: \.self){ item in
                         Text(item)
                             .tag(item)
+                            
                     }
                 }
-                Button("Add Log"){
-                    
-                    dismiss()
+                .accentColor(.white)
+                .bold()
+            }
+            Text("Select instrument")
+                .foregroundStyle(Color(red: 212/255, green: 175/255, blue: 55/255))
+                .bold()
+            Picker("Select Instrument", selection: $selectedInstrument){
+                ForEach(instruments, id: \.self){ item in
+                    Text(item)
+                        .tag(item)
+                }
+                
+            }
+            .accentColor(.white)
+            .bold()
+            Text("Select the amount of time practiced")
+                .foregroundStyle(Color(red: 212/255, green: 175/255, blue: 55/255))
+                .bold()
+        
+            Picker("Select Amount Time", selection: $selectedAmtTime){
+                ForEach(amtOfTimes, id: \.self){ i in
+                    if(i%60 == 0){
+                        Text("\(i/60) hours")
+                            .tag(i)
+                    }else if(Double(i)/60 > 1){
+                        Text("\(Double(i)/60, specifier: "%.1f") hours")
+                            .tag(i)
+                    }else{
+                        Text("\(i) minutes")
+                            .tag(i)
+                    }
                 }
             }
+            
+            Spacer()
+                .frame(width: 1, height: 30)
+            
+            Button("Add Log"){
+                logList.append(Log(day: selectedDay, month: selectedMonth, instrument: selectedInstrument, amtPractice: selectedAmtTime))
+                context.insert(Log(day: selectedDay, month: selectedMonth, instrument: selectedInstrument, amtPractice: selectedAmtTime))
+                try? context.save()
+                dismiss()
+            }
+            .foregroundStyle(Color(red: 212/255, green: 175/255, blue: 55/255))
+            .bold()
+            
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.black)
     }
 }
 
 #Preview {
-    PracticeView(selectedDay: .constant(1), selectedMonth: .constant("January"), logList: .constant([Log(day: 1, month: "January", instrument: "French Horn", amtPractice: 5)]), selectedInstrument: .constant("French Horn"), selectedAmtTime: .constant(5))
+    PracticeView(selectedDay: .constant(1), selectedMonth: .constant("January"), logList: .constant([Log(day: 1, month: "January", instrument: "French Horn", amtPractice: 15)]), selectedInstrument: .constant("French Horn"), selectedAmtTime: .constant(5))
 }
