f42ee672a7eea4e778954356f09fa1030c08882b
diego.chavez27 <noicedude555@gmail.com>
2025-11-17 22:05:39 -0600

    xcodesucks

--- ContentView.swift
+++ ContentView.swift
@@ -8,6 +8,8 @@ struct ContentView: View {
     @State var selectedAmtTime = 15.0
     @State var logList: [Log] = []
     @Query var qLogs: [Log]
+    @State var weeklyStreak = 0
+    @State var dailyStreak = 0
     @Environment(\.modelContext) var context
     var body: some View {
         //Gold: Color(red: 212/255, green: 175/255, blue: 55/255)
@@ -86,6 +88,13 @@ struct ContentView: View {
                 }
                 .foregroundColor(.white)
                 .bold()
+                
+                Text("Weekly streak of \(weeklyStreak)")
+                    .foregroundColor(.white)
+                    .bold()
+                Text("Daily streak of \(dailyStreak)")
+                    .foregroundColor(.white)
+                    .bold()
             }
             .background(Color.black)
         }
@@ -97,5 +106,63 @@ struct ContentView: View {
             context.delete(thing)
             try? context.save()
         }
+    }
+    func calculateStreaks(){
+       
+        if qLogs.count > 1{
+            for i in 0..<qLogs.count-1{
+                if qLogs[i].month == qLogs[i+1].month{
+                    if qLogs[i+1].day - qLogs[i].day >= 7 && qLogs[i+1].day - qLogs[i].day <= 14{
+                        weeklyStreak += 1
+                    }
+                    if qLogs[i+1].day - qLogs[i].day == 1{
+                        dailyStreak += 1
+                    }
+                }
+                if checkMonths(log1: qLogs[i], log2: qLogs[i+1]){
+                    if (qLogs[i+1].day + chckMonth(log: qLogs[i+1])) - qLogs[i].day >= 7 && (qLogs[i+1].day + chckMonth(log: qLogs[i+1])) - qLogs[i].day <= 14{
+                        weeklyStreak += 1
+                    }
+                    if (qLogs[i+1].day + chckMonth(log: qLogs[i+1])) - qLogs[i].day == 1{
+                        dailyStreak += 1
+                    }
+                }
+            }
+        }
+    }
+    func checkMonths(log1: Log, log2: Log)-> Bool{
+        let allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+        var x = 0
+        var y = 0
+        var z = 0
+        for month in allMonths{
+            if(log1.month == month){
+                x = z
+            }
+            if(log2.month == month){
+                y = z
+            }
+            z += 1
+        }
+        if(y - x == 1){
+            return true
+        }else{
+            return false
+        }
+    }
+    func chckMonth(log: Log)-> Int{
+        let thirtyOneMonths = ["January", "March", "May", "July", "August", "October", "December"]
+        let thirtyMonths = ["April", "June", "September", "November"]
+        for month in thirtyOneMonths{
+            if log.month == month{
+                return 31
+            }
+        }
+        for month in thirtyMonths{
+            if log.month == month{
+                return 31
+            }
+        }
+        return 28
     }
 }
